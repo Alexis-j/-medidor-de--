ecc279ef6ff600a376e9abfafd802e48015ac834
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2024-01-19 18:54:01 +0100

    m

--- src/scoring/scoring.js
+++ src/scoring/scoring.js
@@ -68,3 +68,5 @@ function calculateScores() {
 }
 
 module.exports = { calculateScores };
+
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,24 +1,27 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');  // Agrega esta línea para importar el módulo fs
 const { calculateScores } = require('../scoring/scoring.js');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
-// Route for the root, serves an HTML page with a button
+// Configuración para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para la página principal
 app.get('/', (req, res) => {
-  res.send(html);
+  const htmlContent = fs.readFileSync(path.join(__dirname, '../..', 'public', 'index.html'), 'utf-8');
+  res.send(htmlContent);
 });
 
-// Route for /scores
+// Ruta para /scores
 app.get('/scores', (req, res) => {
-  // Calculates scores using the calculateScores function
   const scores = calculateScores();
-
-  // Sends the scores as a JSON response
   res.json(scores);
 });
 
-// Starts the server on the specified port
+// Iniciar el servidor en el puerto especificado
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
